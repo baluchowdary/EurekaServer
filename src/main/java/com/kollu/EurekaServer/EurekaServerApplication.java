1e012b8d2538728e3e26135f749b69d9ae284696
baluchowdary <bhardwaja.kollu@gmail.com>
2021-10-21 00:26:45 +0530

    First commit

---
+++ src/main/java/com/kollu/EurekaServer/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.kollu.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("I am from Eureka Server main");
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
