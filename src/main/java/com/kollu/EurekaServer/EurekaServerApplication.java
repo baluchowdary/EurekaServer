7e4127f4bf4977376aa7cbab1fea165a1315fc75
baluchowdary <bhardwaja.kollu@gmail.com>
2021-10-23 03:36:33 +0530

    Added Distributed Tracing concept - Sleuth, ZipKin, RabbitMQ

--- src/main/java/com/kollu/EurekaServer/EurekaServerApplication.java
+++ src/main/java/com/kollu/EurekaServer/EurekaServerApplication.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -16,5 +19,14 @@ public class EurekaServerApplication {
 		System.out.println("Console:: EurekaServerApplication i am from bank module");
 		logger.info("EurekaServerApplication i am from bank module");
 		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+	
+/*Distributed Tracing/Zipkin*/
+	
+	@Bean
+	public Sampler defaultSampler() {
+		System.out.println("Console:: EurekaServerApplication - defaultSampler method");
+		logger.info("EurekaServerApplication - defaultSampler method");
+	    return Sampler.ALWAYS_SAMPLE;
 	}
 }
